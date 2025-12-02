4148efbf37b94ca67204df7b5745cbb7052d8479
Keytaroko Putra <gheytshakeytaroko@gmail.com>
2025-12-02 15:05:50 +0700

    Add initial HTML structure to index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Hello World Showcase</title>
+  <link rel="stylesheet" href="./style.css">
+</head>
+<body>
+  <div class="container">
+    <h1>Hello World<span class="cursor"></span></h1>
+  </div>
+</body>
+</html>
